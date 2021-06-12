1c965898a1d3d7317a3cfdd139b09f4045e51875
ginia3150 <ginia3150@gmail.com>
2021-06-13 01:27:50 +0900

    作り直し

--- app/models/age.rb
+++ app/models/age.rb
@@ -1,6 +1,6 @@
 class Age < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
+    { id: 1, name: '年齢層を選択' },
     { id: 2, name: '18~29歳' },
     { id: 3, name: '30~49歳' },
     { id: 4, name: '50~64歳' },
